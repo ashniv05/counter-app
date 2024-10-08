2d76d09c7449253997d4c549124a9723f02c5463
ashniv05 <148707601+ashniv05@users.noreply.github.com>
2024-10-08 01:36:49 -0400

    updated html

--- src/counter-app.js
+++ src/counter-app.js
@@ -9,12 +9,18 @@ export class counterApp extends DDDSuper(LitElement) {
 
   constructor() {
     super();
-    this.title = "";
+    this.title = "Counter App";
+    this.counter = 16;
+    this.min = 10;
+    this.max = 25;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      counter: { type: Number },
+      min: { type: Number },
+      max: { type: Number },
     };
   }
 
@@ -36,23 +42,67 @@ export class counterApp extends DDDSuper(LitElement) {
         padding: 0;
         margin: 0;
       }
+      .counter {
+        font-size: 48px;
+        color: black;
+      }
+      .counter.red {
+        color: red;
+      }
+      .counter.green {
+        color: green;
+      }
+      button {
+        font-size: 24px;
+        margin: 8px;
+        padding: 8px 16px;
+      }
+      button:focus {
+        outline: 2px blue;
+      }
     `];
+  }
+  
+  increment() {
+    if (this.counter < this.max) {
+      this.counter++;
+    }
+  }
+  decrement() {
+    if (this.counter > this.min) {
+      this.counter--;
+    }
+  }
+  updated(changedProperties) {
+    if (changedProperties.has('counter')) {
+      if (this.counter === 21) {
+        this.makeItRain();
+      }
+    }
+  }
+  makeItRain() {
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(() => {
+      setTimeout(() => {
+        this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+      }, 0);
+    });
   }
 
   render() {
+    const counterClass = this.counter === 18 
+      ? 'counter green' 
+      : (this.counter === 21 || this.counter === this.max || this.counter === this.min) 
+        ? 'counter red' 
+        : 'counter';
+  
     return html`
-<div class="wrapper">
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
-  }
-
-  /**
-   * haxProperties integration via file reference
-   */
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+      <div class="wrapper">
+        <div class="${counterClass}">${this.counter}</div>
+        <button @click="${this.decrement}" ?disabled="${this.counter === this.min}">-</button>
+        <button @click="${this.increment}" ?disabled="${this.counter === this.max}">+</button>
+        <confetti-container id="confetti"></confetti-container>
+      </div>
+    `;
   }
 }
 
